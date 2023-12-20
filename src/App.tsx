3fc09dd954fb6e0a6c6726f375d9915b0d40d9d0
Alex Santana <analistaalexsantana@gmail.com>
2023-12-20 05:59:26 -0300

    Criando proteção de rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Dashboard } from "./pages/dashboard";
 import { New } from "./pages/dashboard/new";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
+import { Private } from "./routes/Private";
 
 
 
@@ -27,11 +28,11 @@ const router = createBrowserRouter([{
 
       {
         path: "/dashboard",
-        element:<Dashboard />
+        element:<Private><Dashboard /></Private>
       } ,
       {
         path: "/dashboard/new",
-        element:<New />
+        element:<Private><New /></Private>
       } ,
 
    ]
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg'
 import { FiLogIn, FiUser } from 'react-icons/fi'
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export function Header() {
-  const signed = true;
-  const loadingAuth = false;
+  const {signed, loadingAuth} = useContext(AuthContext);
+ 
 
   return (
     <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
